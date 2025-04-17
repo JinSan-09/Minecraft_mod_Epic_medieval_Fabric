0d499739afe6cd199e68e0f9933cdfc4c166e9d1
w1123581321git config --global user.name w1123581321 <p3032237762@163.com>
2025-04-17 12:32:11 +0800

    testOne

---
+++ src/client/java/com/jinsan09/Epic_medievalClient.java
@@ -0,0 +1,10 @@
+package com.jinsan09;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Epic_medievalClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/com/jinsan09/Epic_medievalDataGenerator.java
@@ -0,0 +1,11 @@
+package com.jinsan09;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Epic_medievalDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
